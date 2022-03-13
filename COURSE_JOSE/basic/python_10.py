b69623d3cc98dbcd8ec8097a4b30a56223d1a155
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-03-12 21:06:47 -0300

    new studies

---
+++ COURSE_JOSE/basic/python_10.py
@@ -0,0 +1,12 @@
+# treating strings
+string_with_quotes = "Hello, it's Renata"
+another_string = 'He said You are beautiful'
+
+multiline = """HELLO WORLD. I AM
+A SOFTWARE DEVLOPER"""
+
+print("*******", multiline)
+
+name = "Jose"
+greeting = "Hello, " + name
+print("*******", greeting)
---
+++ COURSE_JOSE/basic/python_11.py
@@ -0,0 +1,12 @@
+# Python string format
+
+age = 34
+name = "Jose"
+age_as_str = str(age)
+
+print("You are" + age)
+
+print("xxxxxxx")
+
+name_presentation = f"My name is {name}"
+print(name_presentation)
