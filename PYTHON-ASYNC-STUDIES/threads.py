92b58f1deb89b7af453115f606ab9228c65f1fd5
renatamonteiro <rcm@lionx.com.br>
2022-03-24 15:45:52 -0300

    adding async code

--- PYTHON-ASYNC-STUDIES/threads.py
+++ PYTHON-ASYNC-STUDIES/threads.py
@@ -1,5 +1,6 @@
 import threading
 import time
+import asyncio
 
 # def new():
 #     while True():
@@ -18,23 +19,58 @@ import time
 
 # -------------
 
-counter = 0
+# counter = 0
 
-def function1():
-    global counter
+# async def function1():
+#     global counter
 
-    while True:
-        counter += 1
-        counter -= 1
+#     while True:
+#         counter += 1
+#         counter -= 1
+#         await asyncio.sleep(0)
 
 
-def function2():
-    global counter
+# async def function2():
+#     global counter
     
-    while True:
-        counter += 1
-        counter -= 1
+#     while True:
+#         counter += 1
+#         counter -= 1
+#         await asyncio.sleep(0)
 
 
-threading.Thread(target=function1).start()
-threading.Thread(target=function2).start()
+# threading.Thread(target=function1).start()
+# threading.Thread(target=function2).start()
+
+# asyncio.gather(function1(), function2())
+# asyncio.get_event_loop().run_forever()
+
+# ----------- using asyncio
+
+"""Getting the documentation to study"""
+
+# async def main():
+#     print("Hello")
+#     await asyncio.sleep(1)
+#     print("world")
+
+# # asyncio.run(main())
+
+# main()
+
+# ------------- examples
+
+async def hello(i):
+    print(f"hello {i} STARTED")
+    await asyncio.sleep(4)
+    print(f"hello {i} DONE")
+
+
+async def main():
+    task1 = asyncio.create_task(hello(1))
+    await asyncio.sleep(3)
+    task2 = asyncio.create_task(hello(2))
+    await task1
+    await task2
+
+asyncio.run(main()) # here's the main loop
