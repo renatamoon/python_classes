ac15a3293ee2f3442cf512ab1aacfd02a503003b
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-03-30 21:33:43 -0300

    while loops

---
+++ COURSE_JOSE/fundamentals/python_27.py
@@ -0,0 +1,8 @@
+# WHILE LOOPS IN PYTHON
+
+is_learning = True
+
+while is_learning:
+    print("YOU'RE LEARNING!")
+    user_input = input("Are you still learning? ").upper
+    is_learning = user_input == "YES"
