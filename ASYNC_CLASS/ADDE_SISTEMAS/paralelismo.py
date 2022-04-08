108263ef7c7309059a6335907fa6f609c3c91bcb
renatamonteiro <r.cardoso.monteiro1@gmail.com>
2022-04-08 10:58:14 -0300

    paralelismo

---
+++ ASYNC_CLASS/ADDE_SISTEMAS/paralelismo.py
@@ -0,0 +1,24 @@
+"""
+PARALELISMO
+
+Imagine que voce quer fazer um jantar para 10 amigos.
+passos:
+
+- lavar os vegetais - 5 minutos de tempo de execução
+- cortar os vegetais - 13 minutos de tempo de execução
+- cozinhar os vegetais - 30 minutos de tempo de execução
+- servir o jantar - 2 minutos de tempo de execução
+
+Vendo esse processo podemos pensar em alguma forma de otimização de processos.
+
+Imagina que voce queira reduzir o cozimento para 250 minutos.
+
+Se alguem começar a te ajudar, logo, a execução em paralelo irá reduzir o tempo de execução.
+
+Mas temos que ver que nesse caso, cada pessoa cozinha em sua propria cozinha, e não disputa um espaço.
+
+Ou seja, o paralelismo acontece quando o computador tem varios "cores". E existem processos rodando em ao menos dois deles.]
+
+O PARALELISMO acontece atraves de execução de diferentes processos.
+
+"""
