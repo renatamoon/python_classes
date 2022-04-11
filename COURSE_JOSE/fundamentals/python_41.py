d4d5236ecc09244472ddce7bb9c0732bc70bf4ab
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-04-11 20:30:17 -0300

    zip function and more

--- COURSE_JOSE/fundamentals/python_41.py
+++ COURSE_JOSE/fundamentals/python_41.py
@@ -17,3 +17,9 @@ print("TIME: ", long_timers) #  {'Rolf': 3, 'Ruth': 7, 'Charlie': 15, 'Jen': 11}
 
 friends_dict = dict(zip(friends_new, time_since_seen))
 print("FRIENDS DICT: ", friends_dict) # {'Rolf': 3, 'Bob': 7, 'Jen': 15, 'Anne': 11}
+
+# ---------------------------------------------------
+# YOU ALSO CAN DO ANOTHER LIST WITH ZIP AND ALSO PUT THE ELEMENTS YOU WANT TO IGNORE
+
+long_timers_zip = list(zip(friends_new, time_since_seen, [1, 2, 3, 4, 5]))
+print("EXCLUDING UNDESIRABLE ELEMENTS: ", long_timers_zip) #  [('Rolf', 3, 1), ('Bob', 7, 2), ('Jen', 15, 3), ('Anne', 11, 4)]
