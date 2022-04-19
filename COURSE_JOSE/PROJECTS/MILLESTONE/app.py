9c59cd28c0913c76c247746c879be049e0c28406
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-04-19 20:36:19 -0300

    project milestone

--- COURSE_JOSE/PROJECTS/MILLESTONE/app.py
+++ COURSE_JOSE/PROJECTS/MILLESTONE/app.py
@@ -1,36 +1,70 @@
-MENU_PROMPT = "Enter 'a' to add a movie, '1' to see your movies, 'f' to find a movie by title, or 'q' to quit: "
+"""PROJECT MILESTONE TO CREATE FUNCTIONS"""
 
-movies = []
+# function to add a movie on the database
 
-title = input("Enter the movie TITLE: ")
-director = input("Enter the movie DIRECTOR: ")
-year = input(int("Enter the movie RELEASE YEAR: "))
+movies_database = [
+    {
+        "title": "James Bond",
+        "director": "Sam Mendes",
+        "year": 2021
+    },
+    {
+        "title": "The Lord Of The Rings",
+        "director": "Peter Jackson",
+        "year": 2001
+    },
+    {
+        "title": "The Devil Wears Prada",
+        "director": "William Lane",
+        "year": 2006
+    }
+]
 
+# def adding_movies_to_library():    
 
-movies.append({
-    "title": title,
-    "director": director,
-    "year": year
-})
+#     title = input("Enter the movie TITLE: ").capitalize()
+#     director = input("Enter the movie DIRECTOR: ").capitalize()
+#     year = input("Enter the movie RELEASE YEAR: ").capitalize()
+
+#     movies_database.append({
+#         "title": title,
+#         "director": director,
+#         "year": year
+#     })
+
+# adding_movies_to_library()
+print(movies_database)
 
 # create other functions for:
 # listening movies
 # finding movies
 
+# def find_movies_on_the_database():
 
-# and another function here for the user menu
-selection = input(MENU_PROMPT)
+title = input("Enter the movie name to find on our database: ").capitalize()
 
-while selection != 'q':
-    if selection == 'a':
-        pass
-    elif selection == 'l':
-        pass
-    elif selection == 'f':
-        pass
+for m in movies_database:
+    if movies_database["title"] == title:
+        print(m)
     else:
-        print("UNKNOWN COMMAND: PLEASE TRY AGAIN!")
+        print("We don't have this movie on our database!")
 
-    selection = input(MENU_PROMPT)
 
-    
+
+# def function_selection_on_the_menu():
+#     MENU_PROMPT = "Enter 'a' to add a movie, '1' to see your movies, 'f' to find a movie by title, or 'q' to quit: "
+
+# # and another function here for the user menu
+#     selection = input(MENU_PROMPT)
+
+#     while selection != 'q':
+#         if selection == 'a':
+#             pass
+#         elif selection == 'l':
+#             pass
+#         elif selection == 'f':
+#             pass
+#         else:
+#             print("UNKNOWN COMMAND: PLEASE TRY AGAIN!")
+
+#         selection = input(MENU_PROMPT)
