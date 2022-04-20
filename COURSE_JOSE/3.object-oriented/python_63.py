fdd2717406adedc1a5fb06a2e5d4ed3ec3262455
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-04-20 20:54:22 -0300

    parameters

--- COURSE_JOSE/3.object-oriented/python_63.py
+++ COURSE_JOSE/3.object-oriented/python_63.py
@@ -1,1 +1,14 @@
 # PARAMETER NAMING
+
+class Movie:
+    def __init__(self, title: str, year: int, director: str) -> dict:
+        self.title = title # properties of self. That's why title is created inside of self. That can be accessed by the . (dot)
+        self.year = year
+        self.director = director
+
+print(Movie('The Matrix', 1999, "Brothers Washowsky").title)
+
+print("-="*20)
+
+movie_1 = Movie('Bewitched', 2004, "Miller Grant").title
+print(movie_1)
