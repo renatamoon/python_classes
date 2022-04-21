5865a6dd99e3b896eeb9b7c385517851f944ae06
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-04-20 21:01:01 -0300

    dunder methods

--- COURSE_JOSE/3.object-oriented/python_64.py
+++ COURSE_JOSE/3.object-oriented/python_64.py
@@ -1,2 +1,31 @@
 # MAGIC METHODS IN PYTHON
 
+from smtplib import SMTPRecipientsRefused
+
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+animals = ["bear", "dog"]
+
+print(animals.__class__) # <class 'list'>
+print(len(animals)) # 2
+
+print("=-"*20)
+
+class Garage:
+    def __init__(self):
+        self.cars = []
+
+    # to get the lenght of the list we need another dunder method
+    def __len__(self):
+        return len(self.cars)
+
+
+ford = Garage()
+ford.cars.append("Fiesta")
+ford.cars.append("Focus")
+
+print("Cars in the garages: ", len(ford))
+print(ford.cars[0]) # fiesta
