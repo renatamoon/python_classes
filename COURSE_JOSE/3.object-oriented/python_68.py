07b04c0aa07b78da52eb8d7123e367cc9d7b75e9
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-04-21 21:18:45 -0300

    class and static methods

--- COURSE_JOSE/3.object-oriented/python_68.py
+++ COURSE_JOSE/3.object-oriented/python_68.py
@@ -8,3 +8,34 @@ class Student:
 
     def average(self):
         return sum(self.marks) / len(self.marks)
+
+# renata = Student('Rolf', 'MIT')
+
+# renata.marks.append(76)
+# renata.marks.append(98)
+
+# print(renata.average())
+
+# ----------
+""" usually you use the class method when you want something to have access to the methods
+of the class"""
+
+class Foo:
+    @classmethod
+    def hi(cls): # the cls, same as self it's holding the value of the class itself
+        print(cls.__name__) # this gives us the name of the class Foo
+        
+my_obj = Foo()
+my_obj.hi()
+
+# ----------
+"""usually you use the static method when you want the method that doesn't use the current object 
+or the class but it's somehow related to it """
+
+class Bar:
+    @staticmethod
+    def hi():
+        print("Hello, I don\'t take parameters.")
+
+another_obj = Bar()
+another_obj.hi()
