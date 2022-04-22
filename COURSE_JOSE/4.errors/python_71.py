be7b45d763634ca99d726be841496137b2f050f1
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-04-22 20:45:51 -0300

    introduction to errors

--- COURSE_JOSE/4.errors/python_71.py
+++ COURSE_JOSE/4.errors/python_71.py
@@ -1,3 +1,15 @@
 # INTRODUCTION TO ERRORS IN PYTHON
 
-print(my_variable)
+print(my_variable) # NameError: name 'my_variable' is not defined
+
+# if I print a variable that is not defined, then I will get an error: NameError
+
+# --------- TIPS THAT WILL HELP TO FIND THE ERROR ANSWERS
+
+"""
+1. look at your code
+2. put the error message into Google and see if something comes up the StackOverflow
+3. Check againt the code line by line
+4. Run only some parts of the code in isolation, this might help you to identify the part that is giving error
+5. use the debugger
+"""
