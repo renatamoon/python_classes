df8952dc2acb602ca4624992e066b745e22013ff
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-04-22 20:56:36 -0300

    built in errors

---
+++ COURSE_JOSE/4.errors/python_72.py
@@ -0,0 +1,16 @@
+# BUILTIN ERROS
+
+"""
+* IndexError
+* KeyError
+* NameError
+* AttributeError:
+* NotImplementedError: 
+* RuntimeError
+* IndentationError
+* TabError
+* TypeError
+* ValueError
+* ImportError
+* DeprecationWarning - 
+"""
