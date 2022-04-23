639cced011a98ea4e1f0d65c1ab885a16b8cd6bc
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-04-23 19:09:35 -0300

    user's score

---
+++ COURSE_JOSE/4.errors/python_73/user_score.py
@@ -0,0 +1,25 @@
+class User:
+    def __init__(self, name, engagement):
+        self.name = name
+        self.engagement_metrics = engagement
+
+    def __repr__(self) -> str:
+        return f'<User {self.name}>'
+
+    
+def get_user_score(user):
+    try:
+        return perform_calculation(user.engagement_metrics)
+    except KeyError:
+        print('Incorrect values were provided to our calculation function')
+
+def perform_calculation(metrics):
+    return metrics['clicks'] * 5 + metrics['hits'] * 2
+
+
+def send_engagement_notification(user):
+    print(f'Notification was sent to the user {user} successfully!')
+
+
+my_user = User('Rolf', {'click': 61, 'hits': 100})
+get_user_score(my_user)
