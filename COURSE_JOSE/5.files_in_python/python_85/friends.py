4fc1a979618ef3cd190fcd8548de946e5b167e41
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-04-25 22:01:20 -0300

    class 85

---
+++ COURSE_JOSE/5.files_in_python/python_85/friends.py
@@ -0,0 +1,8 @@
+# ask the user for a list of 3 friends
+# for each friend, we'll tell the user whether they are nearby
+# for each nearby friend, we'll save their name to 'nearby_friends.txt'
+
+# hint: readlines()
+
+friends = input('Enter 3 names of your friends, separated by comas (no spaces, please)').split(',')
+
