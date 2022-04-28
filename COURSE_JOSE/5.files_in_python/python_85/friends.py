956b92de25e3f34dac22beceb320ae1377119bff
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-04-27 21:46:13 -0300

    reading files

--- COURSE_JOSE/5.files_in_python/python_85/friends.py
+++ COURSE_JOSE/5.files_in_python/python_85/friends.py
@@ -6,3 +6,20 @@
 
 friends = input('Enter 3 names of your friends, separated by comas (no spaces, please)').split(',')
 
+people = open('people.txt', 'r') # mode of reading the file
+people_nearby = [line.strip() for line in people.readlines()]
+
+people.close()
+
+friends_set = set(friends)
+people_nearby_set = set(people_nearby)
+
+friends_nearby = friends_set.intersection(people_nearby_set)
+
+nearby_friends_file = open('nearby_friends.txt', 'w')
+
+for friend in people_nearby_set:
+    print(f'{friend} is nearby! Meet up with me!')
+    nearby_friends_file.write(f'{friend}')
+
+nearby_friends_file.close()
