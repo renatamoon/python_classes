45a4f99395d9267fb621c9950ec6b782cfd0b91f
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-04-29 21:02:41 -0300

    json exercices

--- COURSE_JOSE/5.files_in_python/python_89/json_imports.py
+++ COURSE_JOSE/5.files_in_python/python_89/json_imports.py
@@ -21,3 +21,24 @@ file.close()
 my_json_string = '[{"name": "Alfa Romeo", "released": 1950}]'
 incorrect_car = json.loads(my_json_string)
 print(incorrect_car[0]['name'])
+
+
+# ------------- exercicio
+
+json_list = []      # store the converted json data for each line
+csv_file = open('csv_file.txt', 'r')
+ 
+for line in csv_file.readlines():
+    club, city, country = line.strip().split(',')   # first get rid of the \n and then split with ','
+    data = {
+        'club': club,
+        'city': city,
+        'country': country
+    }
+    json_list.append(data)
+ 
+csv_file.close()
+ 
+json_file = open('json_file.txt', 'w')
+json.dump(json_list, json_file)     # write json data to a file
+json_file.close()
