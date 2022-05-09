d3c9c7226e1d03a85fe9269a523899ee2254aa10
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-05-09 20:33:47 -0300

    adding files

---
+++ COURSE_JOSE/5.files_in_python/python_92/utils/common/find.py
@@ -0,0 +1,12 @@
+from xml.dom import NotFoundErr
+
+
+def find_in(iterable, finder, expected):
+    for i in iterable:
+        if finder(i) == expected:
+            return i
+    raise NotFoundError(f'{expected} not found in provided iterable.')
+
+
+class NotFoundError(Exception):
+    pass
