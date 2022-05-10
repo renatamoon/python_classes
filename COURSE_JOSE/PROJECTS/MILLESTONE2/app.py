857b3b3b310e12d9b3fb1dbd2880c08e9cc4ad46
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-05-09 21:03:09 -0300

    project milestone 2.0

--- COURSE_JOSE/PROJECTS/MILLESTONE2/app.py
+++ COURSE_JOSE/PROJECTS/MILLESTONE2/app.py
@@ -4,18 +4,40 @@ USER_CHOICE = """
 Enter:
 - 'a' to add a new book
 - 'l' to add all books
-- 'r' to mar as read
+- 'r' to mark as read
 - 'd' to delete a book
 - 'q' to quit
 
 YOUR CHOICE: """
 
 
+# function to get the details of a book at the database
+def print_book(book):
+    print(f"Name: {book['name']}")
+    print(f"Author: {book['author']}")
+    print(f"Read: {book['read'].value()}")
+
+
+# function to list all books on the database
+def list_all_movies_on_the_database():
+    for book in database.books:
+        print_book(book)
+
+
+# function to add a book at the database
+def add_book(name, author):
+    database.books.append({'name': name, 'author': author, 'read': False})
+
+
+# function to mark a book as read
+def read_book():
+    pass
+
+
 def menu():
     user_input = input(USER_CHOICE)
     while user_input != 'q':
-        pass
-
+        
 
 # def prompt_add_book() -> ask for book name and author
 # def list_books() -> show all the books in our list
--- COURSE_JOSE/PROJECTS/MILLESTONE2/utils/database.py
+++ COURSE_JOSE/PROJECTS/MILLESTONE2/utils/database.py
@@ -1,7 +1,3 @@
 """CONCERNED WITH STORING AND RETRIEVING BOOKS FROM A LIST"""
 
 books = []
-
-
-def add_book(name, author):
-    books.append({'name': name, 'author': author, 'read': False})
