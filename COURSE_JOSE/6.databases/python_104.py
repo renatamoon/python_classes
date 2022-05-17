712f59364806a59dfca655499081a23d273ed724
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-05-16 21:03:18 -0300

    sql jargons

--- COURSE_JOSE/6.databases/python_104.py
+++ COURSE_JOSE/6.databases/python_104.py
@@ -9,4 +9,11 @@ import sqlite3
 """
 
 connection = sqlite3.connect('data.db')
+
+cursor = connection.cursor()
+
+cursor.execute("YOUR SQL QUERY HERE")
+
+connection.commit()
+
 connection.close()
---
+++ COURSE_JOSE/6.databases/python_106.py
@@ -0,0 +1,11 @@
+# SOME DATABASE JARGON - some therms you need to know to get into relational databases
+
+"""
+- all operations in sqlite are made by cursors, and not by the connection object itself - that
+is why if we have only one connection we can only do a single operation at time. So if we have many cursors
+we can read data easily.
+
+- COMMIT - save the resul of this query to disk,
+- keep a bunch of data in memory until we commit
+- write multiple things together, which is faster
+"""
