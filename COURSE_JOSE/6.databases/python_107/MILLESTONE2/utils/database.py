de02ec1a4fdb56bcdd195a8ebdf0cfca93c2cea9
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2022-05-17 21:00:08 -0300

    adding queries

--- COURSE_JOSE/6.databases/python_107/MILLESTONE2/utils/database.py
+++ COURSE_JOSE/6.databases/python_107/MILLESTONE2/utils/database.py
@@ -1,8 +1,10 @@
 """CONCERNED WITH STORING AND RETRIEVING BOOKS FROM A LIST"""
 import sqlite3
 
+book_file = 'books.json'
 
-# add a book on the database
+
+# creating the table on database
 def create_book_table(name, author):
     connection = sqlite3.connect('data.db')
     cursor = connection.cursor()
@@ -13,9 +15,34 @@ def create_book_table(name, author):
     connection.close()
 
 
+# add book on the database
+def add_book(name, author):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    cursor.execute(f'INSERT INTO books VALUES((?, ?, 0)', (name, author))
+
+    connection.commit()
+    connection.close()
+
+
 # list all books on the database
 def get_all_books():
-    return books
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM books')  # this will give us the entire table, in this case there's
+    # nothing to commit once nothing was save at the disk.
+
+    # and fetchall will give us the rows
+    books_response = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
+
+    connection.close()
+
+    return books_response
+
+
+# update the people table
 
 
 # mark a book as read
